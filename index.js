7a5342be400f1d72406ce48de89b6bb27227a813
diegoa22738 <diego.aguas77@gmail.com>
2016-11-01 18:03:35 +0000

    All my codes

---
+++ index.js
@@ -0,0 +1,18 @@
+  var greeting = "Hello, everybody!";
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `Let\'s talk about ${topic}.`;
+
+var writeGreetingToDocument = ;
+
+// Type your answer into the console without the quote marks and see if it works.
+
+var customCelebration = 'Kaboomie! Hachewie, yeaaaaaaaah. Shwing.';
+
+var consoleCelebration = `something to do with console...`;
+
